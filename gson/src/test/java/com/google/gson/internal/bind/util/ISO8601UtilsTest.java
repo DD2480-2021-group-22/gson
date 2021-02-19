3c4edf8d28cb25c6885e280a5fa358423b3188d3
Binxin <binxin@kth.se>
2021-02-19 13:32:27 +0100

    refactor #1: Fixes bug that leads to failed test.

--- gson/src/test/java/com/google/gson/internal/bind/util/ISO8601UtilsTest.java
+++ gson/src/test/java/com/google/gson/internal/bind/util/ISO8601UtilsTest.java
@@ -17,7 +17,9 @@ public class ISO8601UtilsTest {
 
     @Test
     public void testDateFormatString() {
-        Date date = new GregorianCalendar(2018, Calendar.JUNE, 25).getTime();
+        GregorianCalendar dateG = new GregorianCalendar(2018, Calendar.JUNE, 25);
+        dateG.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = dateG.getTime();
         String dateStr = ISO8601Utils.format(date);
         String expectedDate = "2018-06-25";
         assertEquals(expectedDate, dateStr.substring(0, expectedDate.length()));

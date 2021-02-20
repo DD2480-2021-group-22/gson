2e108c0050422e3fbcfd785da4515043bf293127
Johan Grundberg <johan.grundberg98@gmail.com>
2021-02-21 00:28:48 +0100

    feat #13 Add class for creating a file iff it does not exist, and then appending integers to the file.

---
+++ gson/src/main/java/com/google/gson/FileAppender.java
@@ -0,0 +1,50 @@
+package com.google.gson;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * The FileAppender class is used to append ints as single lines to a file. It is constructed
+ * using a filepath and creates the file at the filepath if needed. Any writes to a closed
+ * FileAppender will result in unspecified behaviour.
+ */
+public class FileAppender {
+
+    // PrintWriter solution largely based on SO question at:
+    // https://stackoverflow.com/questions/4614227/how-to-add-a-new-line-of-text-to-an-existing-file-in-java
+    PrintWriter printWriter;
+
+    /**
+     * Creates a FileAppender which will attempt to write to the file specified.
+     * Creates the file at the filepath if it does not exist.
+     * @param filePath The file to append lines to.
+     */
+    public FileAppender(String filePath) {
+        try {
+            File coverageFile = new File(filePath);
+            coverageFile.createNewFile(); // create file if it does not exist
+            FileWriter fw = new FileWriter(coverageFile, true); // appends to file
+            printWriter = new PrintWriter(fw, true); // automatic flush on
+        } catch (Exception e) {
+            System.out.println("FAILED TO SET UP TEMP FILE WRITER");
+            throw new RuntimeException("FAILED TO SET UP TEMP FILE WRITER", e);
+        }
+    }
+
+    /**
+     * Appends an int to the end of the current file as a single line.
+     * This method will not throw an exception if the FileAppender has been closed.
+     * @param x the integer to write.
+     */
+    public void appendInt(int x) {
+        printWriter.println(x);
+    }
+
+    /**
+     * Closes the underlying PrintWriter. Call when done with this instance.
+     */
+    public void close() {
+        printWriter.close();
+    }
+}

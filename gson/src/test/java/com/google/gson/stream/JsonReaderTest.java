66a51048216251a9d7c5c7fc70e8e86653c61cad
kittyt <kittyt@kth.se>
2021-02-22 12:59:43 +0100

    test #33: Add test to increase branch coverage

--- gson/src/test/java/com/google/gson/stream/JsonReaderTest.java
+++ gson/src/test/java/com/google/gson/stream/JsonReaderTest.java
@@ -298,6 +298,18 @@ public final class JsonReaderTest extends TestCase {
     assertEquals(1L, reader.nextLong());
   }
 
+  /**
+   * Checks if nextInt can read single quoted number
+   * @throws IOException
+   */
+  public void testNextIntSingleQuoted() throws IOException {
+    JsonReader reader = new JsonReader(reader("{ \"number\": '123' }"));
+    reader.setLenient(true);
+    reader.beginObject();
+    assertEquals("number", reader.nextName());
+    assertEquals(123, reader.nextInt());
+  }
+
   public void testDoubles() throws IOException {
     String json = "[-0.0,"
         + "1.0,"

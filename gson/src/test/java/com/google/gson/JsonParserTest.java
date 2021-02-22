87b86c199c02b6061217e2bc2ce7f92ac5d024de
Binxin <binxin@kth.se>
2021-02-22 07:50:01 +0100

    refactor #14: Refactor tests to be more logical

--- gson/src/test/java/com/google/gson/JsonParserTest.java
+++ gson/src/test/java/com/google/gson/JsonParserTest.java
@@ -16,15 +16,14 @@
 
 package com.google.gson;
 
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.StringReader;
-
-import junit.framework.TestCase;
-
 import com.google.gson.common.TestTypes.BagOfPrimitives;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonReader;
+import junit.framework.TestCase;
+
+import java.io.CharArrayReader;
+import java.io.CharArrayWriter;
+import java.io.StringReader;
 
 /**
  * Unit test for {@link JsonParser}
@@ -49,7 +48,7 @@ public class JsonParserTest extends TestCase {
   }
 
   public void testParseString() {
-    String json = "{a:10,b:'c'}";
+    String json = "{a:10,b:'c'}";;
     JsonElement e = JsonParser.parseString(json);
     assertTrue(e.isJsonObject());
     assertEquals(10, e.getAsJsonObject().get("a").getAsInt());
@@ -115,5 +114,97 @@ public class JsonParserTest extends TestCase {
     assertEquals("one", actualOne.stringValue);
     BagOfPrimitives actualTwo = gson.fromJson(element2, BagOfPrimitives.class);
     assertEquals("two", actualTwo.stringValue);
+  }
+
+
+  public void SAMPLEDELETEAFTER() {
+    String json = "f";
+    JsonElement e = JsonParser.parseString(json);
+    assertTrue(e.isJsonObject());
+    assertEquals(10, e.getAsJsonObject().get("a").getAsInt());
+    assertEquals("c", e.getAsJsonObject().get("b").getAsString());
+  }
+
+  /**
+   * Test to increase branch coverage for the method peekNumber(),
+   * Since the method is private, the coverage test have to utilize explicit methods in order to reach them.
+   * found at com/google/gson/stream/JsonReader.java.
+   * The branch is located on line 679.
+   * Requirements that are needed is that the current position in the array buffer is "-",
+   * second condition is that the previous index of the buffer is not 0,
+   * third condition is that is not NUMBER_CHAR_EXP_E,.
+   * If all these 3 conditions holds true, the test will fall to the return statement of PEEKED_NONE.
+   * When PEEKED_NONE is returned, the method will look at the next index of the stack.
+   * The JsonElement.getString() should return correctly parsed string.
+   * Expected, JsonElement == JsonPrimitive and getAsString() = String json.
+   */
+  public void testBranchCoverageLine679() {
+    String json = "1-3";
+    JsonElement e = JsonParser.parseString(json);
+    assertTrue(e.isJsonPrimitive());
+    assertEquals(json,e.getAsString());;
+  }
+
+
+  /**
+   * Test to increase branch coverage for the method peekNumber(),
+   * Since the method is private, the coverage test have to utilize explicit methods in order to reach them.
+   * found at com/google/gson/stream/JsonReader.java.
+   * The branch is located on line 686.
+   * Requirements that are needed is that the current position in the array buffer is "+",
+   * second condition is that the previous index of the buffer is not NUMBER_CHAR_EXP_E,
+   * If all these 2 conditions holds true, the test will fall to the return statement of PEEKED_NONE.
+   * When PEEKED_NONE is returned, the method will look at the next index of the stack.
+   * The JsonElement.getString() should return correctly parsed string.
+   * Expected, JsonElement == JsonPrimitive and getAsString() = String json.
+   */
+  public void testBranchCoverageLine686() {
+      String json = "1+3";
+      JsonElement e = JsonParser.parseString(json);
+      assertTrue(e.isJsonPrimitive());
+      assertEquals(json,e.getAsString());;
+  }
+
+  /**
+   * Test to increase branch coverage for the method peekNumber(),
+   * Since the method is private, the coverage test have to utilize explicit methods in order to reach them.
+   * found at com/google/gson/stream/JsonReader.java.
+   * The branch is located on line 715.
+   * There are alot of requirements for this value since it is the default value in the end -
+   * of a function with Cyclomatic Complexity of 38.
+   * First, it cannot contain any special characters in the previous switch-case list.
+   * Second, it can't be (c < '0' || c > '9') , and it can't be a literal.
+   * Third, previous sign can't be 1 and previous number can't be None.
+   * Fourth, if previous number is the digit of it and the calculated value == 0, then we return PEEKED_NONE.
+   * When PEEKED_NONE is returned, the method will look at the next index of the stack.
+   * Since the String has a leading 0, it will continue parsing since it could be a octal sequence.
+   * The parse omits the leading 0 when converting to int
+   * Expected, JsonElement == JsonPrimitive and getAsInt() = 23
+   */
+  public void testBranchCoverageLine715() {
+    String json = "023";
+    JsonElement e = JsonParser.parseString(json);
+    assertTrue(e.isJsonPrimitive());
+    assertEquals(23,e.getAsInt());;
+  }
+
+  /**
+   * Test to increase branch coverage for the method peekKeyword(),
+   * Since the method is private, the coverage test have to utilize explicit methods in order to reach them.
+   * found at com/google/gson/stream/JsonReader.java.
+   * The branch is located on line 622.
+   * First step is that peekKeyword() checks if the letter can be converted to a keyword.
+   * Second step is that it checks if the length of the char matches the keyword.
+   * If the length of the char is less then allowed buffered depending on the keyword,
+   * and if the pos + i:th positoin of the keyword index is less then the limit for all i:s.
+   * The function will then return PEEKED_NONE, since the keyword does not correspond to the letter.
+   * The sequence then continues until the end of the string. Since it is not a keyword, it will return the string "f".
+   * Expected, JsonElement == JsonPrimitive and String json == e.getAsString()).
+   */
+  public void testBranchCoverageLine622() {
+    String json = "f";
+    JsonElement e = JsonParser.parseString(json);
+    assertTrue(e.isJsonPrimitive());
+    assertEquals(json,e.getAsString());;
   }
 }
